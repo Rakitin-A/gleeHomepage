b4f70b90ea73d31c40e230c329ba241af3f80960
Rakitin-A <74126183+Rakitin-A@users.noreply.github.com>
2021-12-10 14:43:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+
+
+
+    // mixitUP
+    // var mixer = mixitup('.product__items');
+    
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+    
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+    
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
+    //  / mixitUP
+
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true
+    });
+
+
+});
